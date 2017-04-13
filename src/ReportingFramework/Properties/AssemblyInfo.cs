61cd94013ac2bdb2c9446b2a4f489ef5a757d849
frederiksen <mrtn.frederiksen@gmail.com>
2017-04-13 18:08:31 +0200

    Bump version

--- src/ReportingFramework/Properties/AssemblyInfo.cs
+++ src/ReportingFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("mrtn")]
 [assembly: AssemblyProduct("ReportingFramework")]
-[assembly: AssemblyCopyright("Copyright © mrtn 2015")]
+[assembly: AssemblyCopyright("Copyright © mrtn 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.8.0.0")]
-[assembly: AssemblyFileVersion("7.8.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
--- src/TaskCardCreator/Properties/AssemblyInfo.cs
+++ src/TaskCardCreator/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("mrtn")]
 [assembly: AssemblyProduct("TaskCardCreator")]
-[assembly: AssemblyCopyright("Copyright © mrtn 2015")]
+[assembly: AssemblyCopyright("Copyright © mrtn 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.8.0.0")]
-[assembly: AssemblyFileVersion("7.8.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
